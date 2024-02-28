827b024ddeb0721a405826ba61e8b2aa1c7c323f
MattHughson <matthew.hughson@lesappelsillimites.com>
2024-02-27 21:11:18 -0500

    added the env

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import FieldPlugin from './components/FieldPlugin'
-import FieldPluginExample from './components/FieldPluginExample'
 import { FunctionComponent } from 'react'
-
+import { useFieldPlugin } from '@storyblok/field-plugin/react'
+ 
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material'
+import { lightTheme } from '@storyblok/mui'
+ 
 const App: FunctionComponent = () => {
-  return <FieldPluginExample />
+  const { type, error } = useFieldPlugin()
+  
+  if (type === 'loading') {
+    return <Loading />
+  } else if (type === 'error') {
+    return <Error error={error} />
+  } else {
+    return (
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <GlobalStyles
+          styles={{
+            body: { backgroundColor: 'transparent' },
+          }}
+        />
+        <FieldPlugin />
+      </ThemeProvider>
+    )
+  }
 }
-
+ 
+const Loading: FunctionComponent = () => <p>Loading...</p>
+const Error: FunctionComponent<{ error: Error }> = (props) => {
+  console.error(props.error)
+  return <p>An error occured, please see the console for more details.</p>
+}
 export default App
